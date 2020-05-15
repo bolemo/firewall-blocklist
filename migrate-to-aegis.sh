79f561f59cee0df2cfdccac2f5d0a537caeffb6d
bolemo <grvg@free.fr>
2020-05-15 12:20:09 +0200

    Create migrate-to-aegis.sh

---
+++ migrate-to-aegis.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+/opt/bolemo/script/firewall-blocklist clean
+rm -f /opt/bolemo/script/firewall-blocklist
+[ -e /opt/bolemo/etc/firewall-blocklist.sources ] && mv /opt/bolemo/etc/firewall-blocklist.sources /opt/bolemo/etc/aegis.sources
+[ -e /opt/bolemo/etc/firewall-blocklist.sources ] && mv /opt/bolemo/etc/firewall-blocklist.sources /opt/bolemo/etc/aegis.custom-blacklist.netset
+[ -e /opt/bolemo/etc/firewall-blocklist.sources ] && mv /opt/bolemo/etc/firewall-blocklist.sources /opt/bolemo/etc/aegis.custom-blacklist.netset
+[ -e /opt/bolemo/etc/firewall-blocklist-bl.netset ] && rm -f /opt/bolemo/etc/firewall-blocklist-bl.netset
+[ -e /opt/bolemo/etc/firewall-blocklist-wl.netset ] && rm -f /opt/bolemo/etc/firewall-blocklist-wl.netset
