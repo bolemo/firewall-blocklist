da57bded182e9df4d26c4999fb4ef795bb168297
bolemo <grvg@free.fr>
2020-05-18 12:22:18 +0200

    Update test-vpn.sh

--- test-vpn.sh
+++ test-vpn.sh
@@ -3,7 +3,7 @@ WAN_IF="$(ip route | sed -n "s/.*via $WAN_IP dev \([[:alnum:]]*\).*/\1/p")"
 if ifconfig|grep -qF ‘wg0’; then
     TUN_IF=‘wg0’
 elif ps -w|grep -vE ‘grep|server_tap|server_tun’|grep -qF -- ‘/usr/sbin/openvpn --dev’; then
-    TUN_IF="$(ps -w|grep -F -- ‘/usr/sbin/openvpn --dev’)"|grep -vE ‘grep|server_tap|server_tun’|sed -n -- "s/.*openvpn --dev \([[:alnum:]]*\).*/\1/p")"
+    TUN_IF="$(ps -w|grep -F -- ‘/usr/sbin/openvpn --dev’|grep -vE ‘grep|server_tap|server_tun’|sed -n -- 's/.*openvpn --dev \([[:alnum:]]*\).*/\1/p')"
 else
     TUN_IF=‘’
 fi
