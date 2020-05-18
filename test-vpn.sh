e4cbd6787416280882990c0a729901cb380b650e
bolemo <grvg@free.fr>
2020-05-18 20:00:36 +0200

    Update test-vpn.sh

--- test-vpn.sh
+++ test-vpn.sh
@@ -1,5 +1,6 @@
-WAN_IP="$(nvram get wan_gateway)"
-WAN_IF="$(ip route | sed -n "s/.*via $WAN_IP dev \([[:alnum:]]*\).*/\1/p")"
+WAN_IF1="$(nvram get wan_ifname)"
+ifconfig ppp0 >/dev/null 2>/dev/null && WAN_IF2='ppp0' || WAN_IF2='brwan'
+ip route | grep -qF 'ppp0' && WAN_IF3='ppp0' || WAN_IF3='brwan'
 if ifconfig|grep -qF 'wg0'; then
     TUN_IF='wg0'
 elif ps -w|grep -vE 'grep|server_tap|server_tun'|grep -qF -- '/usr/sbin/openvpn --dev'; then
@@ -7,6 +8,8 @@ elif ps -w|grep -vE 'grep|server_tap|server_tun'|grep -qF -- '/usr/sbin/openvpn 
 else
     TUN_IF=''
 fi
-echo "WAN Interface name: $WAN_IF"
+echo "WAN Interface name method 1: $WAN_IF1"
+echo "WAN Interface name method 2: $WAN_IF2"
+echo "WAN Interface name method 3: $WAN_IF3"
 [ "$TUN_IF" ] && echo "VPN Interface name: $TUN_IF" || echo 'No VPN or Wireguard (client) detected'
 echo 'Thank you for testing :-)'
