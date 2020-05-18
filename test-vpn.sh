945bfaa3f2d03f2f37a0258eb03e027994f0b2cb
bolemo <grvg@free.fr>
2020-05-18 12:37:35 +0200

    Update test-vpn.sh

--- test-vpn.sh
+++ test-vpn.sh
@@ -1,12 +1,11 @@
-#!/dev/sh
 WAN_IP="$(nvram get wan_gateway)"
 WAN_IF="$(ip route | sed -n "s/.*via $WAN_IP dev \([[:alnum:]]*\).*/\1/p")"
-if ifconfig|grep -qF ‘wg0’; then
-    TUN_IF=‘wg0’
-elif ps -w|grep -vE ‘grep|server_tap|server_tun’|grep -qF -- ‘/usr/sbin/openvpn --dev’; then
-    TUN_IF="$(ps -w|grep -F -- ‘/usr/sbin/openvpn --dev’|grep -vE ‘grep|server_tap|server_tun’|sed -n -- 's/.*openvpn --dev \([[:alnum:]]*\).*/\1/p')"
+if ifconfig|grep -qF 'wg0'; then
+    TUN_IF='wg0'
+elif ps -w|grep -vE 'grep|server_tap|server_tun'|grep -qF -- '/usr/sbin/openvpn --dev'; then
+    TUN_IF="$(ps -w|grep -F -- '/usr/sbin/openvpn --dev'|grep -vE 'grep|server_tap|server_tun'|sed -n -- 's/.*openvpn --dev \([[:alnum:]]*\).*/\1/p')"
 else
-    TUN_IF=‘’
+    TUN_IF=''
 fi
 echo "WAN Interface name: $WAN_IF"
 [ "$TUN_IF" ] && echo "VPN Interface name: $TUN_IF" || echo 'No VPN or Wireguard (client) detected'
